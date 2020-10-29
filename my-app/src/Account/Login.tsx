34b3c3fc4461ba2aff37a675e1c8b22d19b2a816
Emirhan <Demirci.Emirhan@outlook.com>
2020-10-29 15:52:21 +0100

    login images added

--- my-app/src/Account/Login.tsx
+++ my-app/src/Account/Login.tsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Logo from "../images/isaac-primary-logo-rgb-transparent-250px.png";
 import './Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { textChangeRangeIsUnchanged } from 'typescript';
 class Login extends Component {
-
+    
     // constructor(props) {
     //   super(props);
     //   this.userLoaded = this.userLoaded.bind(this); 
@@ -27,14 +27,13 @@ class Login extends Component {
     NotAuthenticated() {
         return <div>You are not authenticated, please click here to authenticate.</div>;
     }
-
     render() {
         return (
             <Card style={{width: '25rem', marginLeft: 'auto', marginRight: 'auto', marginTop: '18%', backgroundColor: 'white'}}>
-                <Card.Header style={{width: '25rem'}}>Login</Card.Header>
+                <Card.Img variant="top" src={Logo}></Card.Img>
+                <Card.Header>Welcome back! Login to acces the ISAAC heatmap</Card.Header>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
@@ -45,11 +44,8 @@ class Login extends Component {
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
+                    <Button size="lg" style={{marginLeft: '42%'}} variant="primary" type="submit">
+                        Login
                     </Button>
                 </Form>
             </Card>
