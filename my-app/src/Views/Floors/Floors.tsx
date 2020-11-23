dbe94d770227fad2beb3f15e94366c255c4508c9
Emirhan <Demirci.Emirhan@outlook.com>
2020-11-23 14:30:26 +0100

    changes Marco

--- my-app/src/Views/Floors/Floors.tsx
+++ my-app/src/Views/Floors/Floors.tsx
@@ -5,17 +5,17 @@ import AddFloor from "./Components/AddFloor";
 import DropDown from "./Components/DropDown";
 
 class FloorsPage extends Component {
-	render() {
-		return (
-			<div>
-				<h2 className="headerpos">Floor</h2>
-				<DropDown />
-				<AddFloor />
-				<h2 className="headerpos">Sensors</h2>
-				<SensorData />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h2 className="headerpos">Floor</h2>
+        <DropDown />
+        <AddFloor />
+        <h2 className="headerpos">Sensors</h2>
+        <SensorData />
+      </div>
+    );
+  }
 }
 
 export default FloorsPage;
