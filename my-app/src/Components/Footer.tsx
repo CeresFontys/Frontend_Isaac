938e109f7db7f1fd7662e49ce01014d9d6af6b42
MarcoWeel <deathstarmarco@gmail.com>
2021-01-15 13:36:24 +0100

    changed footer added redirect

--- my-app/src/Components/Footer.tsx
+++ my-app/src/Components/Footer.tsx
@@ -1,45 +1,44 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./Footer.css";
 import AuthService from "../Views/Account/services/auth.service";
-import jwt_decode from "jwt-decode";
-import { User } from "../Views/Account/models/User";
-// import { Container } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// import { Navbar } from "react-bootstrap";
-// import { NavbarBrand } from "react-bootstrap";
 import logoutIcon from "../Media/icons/logout.png";
 import UserIcon from "../Media/icons/userIcon.png";
+import { Redirect } from "react-router-dom";
 
-class Footer extends Component {
-  render() {
-    let userstatus = false;
-    let user;
-    if (localStorage.getItem("user") != undefined) {
-      user = JSON.parse(localStorage.getItem("user"));
-      userstatus = true;
-      console.log(user);
-      console.log(user.user.email);
-    } else {
-      user = "No user";
-    }
-
-    if (window.location.pathname === "/login") return null;
-    return (
-      <div id="Footer">
-        <div className="inline">
-          <img className="userIcon" src={UserIcon}></img>
-          {userstatus && <div className="userEmailText">{user.user.email}</div>}
-        </div>
-        <img
-          className="logoutIcon"
-          onClick={() => {
-            AuthService.logout();
-          }}
-          src={logoutIcon}
-        ></img>
-      </div>
-    );
+function Footer() {
+  const [redirect, setRedirect] = useState(false);
+  const handleClick = () => {
+    AuthService.logout();
+    setRedirect(true);
+  };
+  let userstatus = false;
+  let user;
+  if (localStorage.getItem("user") != undefined) {
+    user = JSON.parse(localStorage.getItem("user"));
+    userstatus = true;
+    console.log(user);
+    console.log(user.user.email);
+  } else {
+    user = "No user";
   }
+
+  if (window.location.pathname === "/login") return null;
+  return (
+    <div id="Footer">
+      <div className="inline">
+        <img className="userIcon" src={UserIcon}></img>
+        {userstatus && <div className="userEmailText">{user.user.email}</div>}
+      </div>
+      <img
+        className="logoutIcon"
+        onClick={() => {
+          handleClick();
+        }}
+        src={logoutIcon}
+      ></img>
+      {redirect && <Redirect push to="/login" />}
+    </div>
+  );
 }
 
 export default Footer;
